7968d0ce5b78b971f69219242ad35dd6916e533a
BigCircle <zhuceafter2015@163.com>
2019-08-15 22:39:56 -0400

    quick sort in plave

--- 7.2.1must-have-knowledge/quick_sort_inPlace.py
+++ 7.2.1must-have-knowledge/quick_sort_inPlace.py
@@ -5,6 +5,7 @@ import random
 
 # init
 rand_array=[int(random.random()*100)]
+#rand_array=[9, 8, 7, 6, 5, 4, 3, 2, 1]
 for i in range(1,10):
     rand_array.append(random.randrange(1,1000))
 
@@ -12,24 +13,20 @@ print("Initial random array is:", rand_array)
 
 def swap(array, index1, index2):
     array[index1], array[index2] = array[index2], array[index1]
-    return array
 
 # partition
 def partition(array, left_index, right_index, pivot_value):
 
-    print("a:",array)
-    print("p:",pivot_value)
-    print("left:", left_index)
-    print("r:",right_index)
     while(left_index < right_index):
         while(array[left_index] < pivot_value):
             left_index += 1
-            print("left-:", left_index)
-        while(array[right_index] > pivot_value):
+            #print("left -:", left_index)
+        while( (array[right_index] > pivot_value) and right_index > 0):
             right_index -= 1
-            print("r-:",right_index)
+            #print("right -:",right_index)
         if left_index < right_index:
             swap(array, left_index, right_index)
+    #print("return left index:", left_index)
     return left_index
 
 
@@ -37,12 +34,10 @@ def partition(array, left_index, right_index, pivot_value):
 def quicksort(init_array, left=0, right=None):
     if right == None:
         right = len(init_array) - 1
-    print("q l:",left)
-    print("q r:",right)
-    if left < right:
-        pivot_value = init_array[(right - left) // 2]
-        print("q p:",pivot_value)
+    if left < right - 1:
+        pivot_value = init_array[left + ((right - left) // 2)]
         mid_index = partition(init_array, left, right, pivot_value)
+        #print("input left mid_index right:", left, mid_index, right)
         quicksort(init_array, left, mid_index - 1)
         quicksort(init_array, mid_index, right)
 
